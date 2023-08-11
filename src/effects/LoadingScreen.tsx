90ed1a5226f6efe1fce58cb67080d17027dc9023
myzticx <navi.juss@gmail.com>
2023-08-11 19:14:19 +0100

    checking what went wrong

---
+++ src/effects/LoadingScreen.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./LoadingScreen.css";
+
+interface LoadingScreenProps {
+  children: React.ReactNode;
+}
+
+const LoadingScreen: React.FC<LoadingScreenProps> = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isInitialLoading, setIsInitialLoading] = useState(true); // New state
+
+  useEffect(() => {
+    // Simulate a delay for demonstration purposes
+    setTimeout(() => {
+      setIsLoading(false); // Hide the loading screen
+      setIsInitialLoading(false); // Set initial loading state to false
+    }, 3000); // Adjust the duration as needed
+  }, []);
+
+  return (
+    <div className="loading-screen">
+      {isInitialLoading ? (
+        <div className="loading-container">
+          <div className="loading-spinner"></div>
+          <div className="loading-text">Loading...</div>
+        </div>
+      ) : isLoading ? (
+        <div className="loading-text">Loading...</div>
+      ) : (
+        children
+      )}
+    </div>
+  );
+};
+
+export default LoadingScreen;
--- src/sections/homepage.tsx
+++ src/sections/homepage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./homepage.css";
 import { styled } from "styled-components";
+import LoadingScreen from "../effects/LoadingScreen";
+import "../App.css";
 
 const SectionBackground = styled.div`
   background-color: rgb(30, 20, 82);
@@ -9,90 +11,100 @@ const SectionBackground = styled.div`
 
 export function HomePageSection() {
   return (
-    <SectionBackground>
-      <div
-        data-collapse="medium"
-        data-animation="default"
-        data-duration="400"
-        data-easing="ease"
-        data-easing2="ease"
-        role="banner"
-        className="navigation-transparent w-nav"
-      >
-        <div className="container w-container">
-          <a
-            href="/"
-            aria-current="page"
-            className="aller-logo w-nav-brand w--current"
-            aria-label="home"
-          >
-            <img
-              src="https://cdn.discordapp.com/attachments/1103327962981470359/1139297141144166551/image-removebg-preview_13.png"
-              className="logo-white"
-            />
-          </a>
-          <nav role="navigation" className="nav-menu w-nav-menu">
+    <LoadingScreen>
+      <div className="content"></div>
+      <SectionBackground>
+        <div
+          data-collapse="medium"
+          data-animation="default"
+          data-duration="400"
+          data-easing="ease"
+          data-easing2="ease"
+          role="banner"
+          className="navigation-transparent w-nav"
+        >
+          <div className="container w-container">
             <a
-              href="./homepage.tsx"
-              className="nav-link-transparent w-nav-link w--current"
+              href="/"
+              aria-current="page"
+              className="aller-logo w-nav-brand w--current"
+              aria-label="home"
             >
-              HOME
+              <img
+                src="https://cdn.discordapp.com/attachments/714850955643977769/1139534937540796577/image-removebg-preview_15.png"
+                className="logo-white"
+              />
             </a>
-            <a href="./about.tsx" className="nav-link-transparent w-nav-link">
-              About
-            </a>
-            <a href="#what-we-do" className="nav-link-transparent w-nav-link">
-              What We Do
-            </a>
-            <a
-              href="#who-we-work-with"
-              className="nav-link-transparent w-nav-link"
+            <nav role="navigation" className="nav-menu w-nav-menu">
+              <a
+                href="./homepage.tsx"
+                className="nav-link-transparent w-nav-link w--current"
+              >
+                HOME
+              </a>
+
+              <a href="./about.tsx" className="nav-link-transparent w-nav-link">
+                About us
+              </a>
+              <a href="#what-we-do" className="nav-link-transparent w-nav-link">
+                What We Do
+              </a>
+              <a
+                href="#who-we-work-with"
+                className="nav-link-transparent w-nav-link"
+              >
+                STYLED COMPONENTS
+              </a>
+              <a
+                href="#leadership-team"
+                className="nav-link-transparent w-nav-link"
+              >
+                REACT-SOCKS
+              </a>
+              <a
+                href="#work-with-us"
+                className="nav-link-transparent w-nav-link"
+              >
+                INTL-UNIVERSAL
+              </a>
+              <a href="#contact" className="nav-link-transparent w-nav-link">
+                REACT APP
+              </a>
+              <a href="#contact" className="nav-link-transparent w-nav-link">
+                MORE
+              </a>
+            </nav>
+            <div
+              className="menu-button w-nav-button"
+              aria-label="menu"
+              role="button"
+              tabIndex={0}
+              aria-controls="w-nav-overlay-1"
+              aria-haspopup="menu"
+              aria-expanded="false"
             >
-              STYLED COMPONENTS
-            </a>
-            <a
-              href="#leadership-team"
-              className="nav-link-transparent w-nav-link"
-            >
-              REACT-SOCKS
-            </a>
-            <a href="#work-with-us" className="nav-link-transparent w-nav-link">
-              INTL-UNIVERSAL
-            </a>
-            <a href="#contact" className="nav-link-transparent w-nav-link">
-              REACT APP
-            </a>
-          </nav>
-          <div
-            className="menu-button w-nav-button"
-            aria-label="menu"
-            role="button"
-            tabIndex={0}
-            aria-controls="w-nav-overlay-1"
-            aria-haspopup="menu"
-            aria-expanded="false"
-          >
-            <div className="icon-3 w-icon-nav-menu"></div>
+              <div className="icon-3 w-icon-nav-menu"></div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="centered-container hero">
-        <style>
-          @import
-          url('https://fonts.googleapis.com/css2?family=Berkshire+Swash&family=Bricolage+Grotesque:opsz,wght@10..48,200&family=Comfortaa:wght@500;700&family=Mochiy+Pop+P+One&family=Poppins:wght@300;400;500;600;700;800;900&family=Rajdhani&family=Roboto+Mono:wght@100&family=Ubuntu+Condensed&display=swap');
-        </style>
+        <div className="centered-container hero">
+          <style>
+            @import
+            url('https://fonts.googleapis.com/css2?family=Berkshire+Swash&family=Bricolage+Grotesque:opsz,wght@10..48,200&family=Comfortaa:wght@500;700&family=Mochiy+Pop+P+One&family=Poppins:wght@300;400;500;600;700;800;900&family=Rajdhani&family=Roboto+Mono:wght@100&family=Ubuntu+Condensed&display=swap');
+          </style>
 
-        <h1 className="hero-heading">
-          Having trouble using react? Well you've stumbled upon the right
-          webpage. Here at ReactRamp we have examples on how to use certain uses
-          of React
-        </h1>
-        <div className="hero-text">Thank you for looking at ReactRamp!</div>
-        <a href="" className="button-2 yellow w-button">
-          Get Started
-        </a>
-      </div>
-      {/* </header> */}
-    </SectionBackground>
+          <h1 className="hero-heading">
+            Having trouble using React? Well you've stumbled upon the right
+            website. Here at ReactRamp we have examples on how to use certain
+            uses of React
+          </h1>
+          <div className="hero-text">Thank you for visiting ReactRamp!</div>
+          <a href="" className="button-2 yellow w-button">
+            Get Started
+          </a>
+        </div>
+        {/* </header> */}
+      </SectionBackground>
+    </LoadingScreen>
   );
 }
