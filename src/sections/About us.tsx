b1628cec92628798136494237cd48bce3b0005ec
Manmohn singh Rathoure <singhmanmohn87@gmail.com>
2023-08-11 14:02:01 +0100

    inital commit

---
+++ src/sections/About us.tsx
@@ -0,0 +1,9 @@
+import react from "react";
+import "./About us.css";
+import { styled } from "styled-components";
+
+const MainHeader = styled.header;
+
+export function AboutUs() {
+  return <header>About Us</header>;
+}
