f217947c1d0c1354e72ff3cec33d21f84584f4c7
myzticx <navi.juss@gmail.com>
2023-08-11 12:44:01 +0100

    didnt like about us page, to be modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { HomePageSection } from "./sections/homepage";
-import { AboutUs } from "./sections/about";
 
 function App() {
   return (
     <div>
       <HomePageSection />
-      <AboutUs />
     </div>
   );
 }
